0121f97b333607773c887489802d0010d7d854a8
Dasha R <nameoffox@yandex.ru>
2022-10-01 14:49:50 +0300

    start game

--- game_v3.py
+++ game_v3.py
@@ -10,16 +10,24 @@ def random_predict(number:int=1) -> int:
         int: Число попыток
     """
 
-    count = 0
+    count = 1
+    number_guessed_lesser = 0
+    number_guessed_larger = 101
+    predict_number = 50
 
-    while True:
+    while predict_number != number:
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
-        if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
-
-#print(f'Количество попыток: {random_predict()}')
+        # предполагаемое число
+        if predict_number < number:
+           number_guessed_lesser = predict_number
+        else: number_guessed_larger = predict_number
+        predict_number = (number_guessed_larger - number_guessed_lesser)//2 + number_guessed_lesser
+        print(f"number_guessed_larger {number_guessed_larger}")
+        print(f"number_guessed_lesser {number_guessed_lesser}")
+        print(f"predict_number {predict_number}")
+        print(f"number {number}")
+    return count
+print(f'Количество попыток: {random_predict()}')
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
@@ -30,18 +38,20 @@ def score_game(random_predict) -> int:
     Returns:
         int: среднее количество попыток
     """
-    print(f'Старт:')
+
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
+    n=0
     for number in random_array:
+        n += 1
+        print (f"Занесли число {number}, {n} раз прошёл цикл")
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
+    return score
 
 
 if __name__ == "__main__":
